961d761dc17b271f2880702ada62bd050d1d9604
endreo123 <endreo.leoni@gmail.com>
2023-06-15 15:22:03 -0300

    aula 2

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import AppRouter from 'routes';
+
+const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Cardapio />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AppRouter />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
