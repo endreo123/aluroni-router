517d2c3c03dec8a00d85102b2b91a15c3495325f
endreo123 <endreo.leoni@gmail.com>
2023-06-16 17:39:35 -0300

    aula 3

---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import Header from 'components/Header';
+import Menu from 'components/Menu';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+export default function AppRouter() {
+    return (
+        <main>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path='/' element={<Header />}>
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </main>
+    );
+}
