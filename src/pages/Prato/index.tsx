7810d3e05a771c83ded933e91329c73e2628d6fc
endreo123 <endreo.leoni@gmail.com>
2023-06-19 14:18:03 -0300

    aula 5

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,7 +1,7 @@
-import classNames from 'classnames';
 import styles from './Prato.module.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
     const { id } = useParams();
@@ -13,12 +13,6 @@ export default function Prato() {
             </div>
         </>;
     }
-
-    const categoryLabelString = prato.category.label.toLowerCase();
-    const categoryLabel = classNames({
-        [styles.tags__tipo]: true,
-        [styles[`tags__tipo__${categoryLabelString}`]]: true
-    });
 
     const navigate = useNavigate();
 
@@ -34,25 +28,7 @@ export default function Prato() {
                 <div className={styles.imagem}>
                     <img src={prato.photo} alt={prato.title} />
                 </div>
-                <div className={styles.conteudo}>
-                    <p className={styles.conteudo__descricao}>
-                        {prato.description}
-                    </p>
-                    <div className={styles.tags}>
-                        <div className={categoryLabel}>
-                            {prato.category.label}
-                        </div>
-                        <div className={styles.tags__porcao}>
-                            {prato.size}g
-                        </div>
-                        <div className={styles.tags__qtdpessoas}>
-                            Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 's'}
-                        </div>
-                        <div className={styles.tags__valor}>
-                            R$ {prato.price.toFixed(2).replace('.', '/')}
-                        </div>
-                    </div>
-                </div>
+                <TagsPrato {...prato} />
             </section>
         </>
     );
